a95527cd33130dda1eca43c86b0f7ac73cab58a4
cerbotx9 <53687125+cerbotx9@users.noreply.github.com>
2019-10-04 03:25:24 -0500

    Add files via upload

---
+++ CTDT/VISTA/PHP/cabecera.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="VISTA/PHP/vendor/css/main.css" >
+    
+    <title>Leyendas Misterio y Tradici&oacute;n</title></head>
+
+    <body>
+   
+   <!-- Barra de Navegacion -->
+   <nav class="navbar navbar-dark navbar-dark bg-dark fixed-top">
+     <!-- Navbar content -->
+     <a class="navbar-brand" href="index.php">Contra Tiempo Danza y Teatro 2019</a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
+     <div class="collapse navbar-collapse" id="navbarNav">
+       <ul class="navbar-nav">
+         <li class="nav-item active">
+           <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link" href="#Contacto">Contacto</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link" href="#Nosotros">Sobre Nosotros</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">2019 @Contra Tiempo Danza y Teatro</a>
+         </li>
+       </ul>
+     </div>
+   </nav>
+   
+   <!-- Barra de Navegacion -->
---
+++ CTDT/index.php
@@ -0,0 +1,100 @@
+<?php include 'VISTA/PHP/cabecera.php'; ?>
+
+<header class="bg-warning text-white">
+    <div class="container text-center">
+      <h1>SE PARTE DE:</h1>
+      <p class="lead"> LEYENDAS ...</p>
+    </div>
+  </header>
+
+<hr/>
+    <div class="container text-center">
+                <div class="card mb-6" >
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                    <img  style="max-width: 1350px; height: 350px; "  src="https://bahaiteachings.org/wp-content/uploads/2019/04/Quetzalcoatl.jpg" class="card-img" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Misterio y Tradici&oacute;n.</h5>
+                        <p class="card-text">
+
+                        Con el propósito de recopilar diversas leyendas y mitos de la cultura mexicana, se ha credo un 
+                        espectáculo escénico que narra múltiples sucesos, historias fantásticas donde se mezclan; 
+                        <hr>
+                        Mitología y realidad, hombres, dioses, vivos y muertos, personajes históricos e imágenes 
+                        de la grafica popular. Con la finalidad de  transmitir las diversas creencias acerca del 
+                        "más allá" a través de diversas generaciones,  revelando la relación con su entorno durante 
+                        las distintas épocas que han conformado nuestro imaginario social. Tradiciones que logran reunir 
+                        a las familias, abuelos, hijos, nietos, quienes escucharon de los ancianos el eco del  pasado
+                        que nos heredan, y nos da sentido de identidad. 
+
+                </p>
+                        <p class="card-text"><small class="text-muted">Clasificaci&oacute;n Leyendas Mexicanas</small></p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <br>
+    </div>
+
+
+<div class="container text-center">
+
+
+
+        <div class="row">
+
+                    <div class="col-lg-6 col-md-6 mb-6">
+                        <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="https://bahaiteachings.org/wp-content/uploads/2019/04/quetzalcoatl-and-indigenous-spiritual-truth-bahai.jpg" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                            <a href="#">El Nahuatl</a>
+                            </h4>
+                            <h5>Subtitulo</h5>
+                            <p class="card-text">
+                            leyenda de terror de fines del siglo XVIII, habla de la terrible  transformaci&oacute;n de hombres
+                            en bestias  que andan al acecho (duraci&oacute;n 5 minutos).
+                                </p>
+                        </div>
+                        <div class="card-footer">
+
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 mb-9">
+                        <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="https://bahaiteachings.org/wp-content/uploads/2019/04/quetzalcoatl-and-indigenous-spiritual-truth-bahai.jpg" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                            <a href="#">Francisca y la Muerte</a>
+                            </h4>
+                            <h5>Subtitulo</h5>
+                            <p class="card-text">
+                            leyenda en tono de comedia del siglo XIX, acerca de una mujer anciana 
+                            activa que se burla de la Muerte  (duraci&oacute;n 15 minutos).
+                            </p>
+                        </div>
+                        <div class="card-footer">
+
+                        </div>
+                        </div>
+                    </div>
+
+
+
+
+                    
+        </div>
+        <!-- /.row -->
+
+
+<br>
+</div>
+<!-- /.container -->
+<hr/>
+<br><br><br>
+<h3>@Cerbotx Developers</h3>
+
+<?php include 'VISTA/PHP/piePagina.php'; ?>
